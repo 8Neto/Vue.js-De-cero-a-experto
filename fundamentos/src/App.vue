5893ba6de45a87bec67e50903c58bd02ee2120fa
Neto <neto08_cruz@hotmail.com>
2023-01-07 19:41:04 -0600

    feat: :sparkles: Primer componente

--- fundamentos/src/App.vue
+++ fundamentos/src/App.vue
@@ -1,21 +1,26 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="TEST!" />
+      <Counter/>
     </div>
   </header>
 
   <main>
-    <TheWelcome />
   </main>
 </template>
+
+<script>
+import Counter from '@/components/Counter.vue'
+
+export default {
+  name:'App',
+  components:{
+    Counter
+  }
+}
+</script>
 
 <style scoped>
 header {
