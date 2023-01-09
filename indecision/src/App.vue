7ae3354c3d33b7e4da570b78837123bf9a043230
Neto <neto08_cruz@hotmail.com>
2023-01-08 18:43:05 -0600

    feat: :sparkles: Indecision component

---
+++ indecision/src/App.vue
@@ -0,0 +1,22 @@
+<script>
+  import Indecision from '@/components/Indecision.vue'
+
+  export default {
+    name: 'App',
+    components: {
+      Indecision
+    }
+  }
+</script>
+
+<template>
+  <header>
+  </header>
+
+  <main>
+    <Indecision />
+  </main>
+</template>
+
+<style scoped>
+</style>
