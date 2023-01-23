8a69ae80c5f86760f1da490b6993061be9e556d7
Neto <neto08_cruz@hotmail.com>
2023-01-23 15:46:28 -0600

    feat: :sparkles: Get Pokemon Names
    Install axios and use helpers

---
+++ pokemon-app/src/helpers/getPokemonOptions.js
@@ -0,0 +1,33 @@
+import pokemonApi from '../api/pokemonApi'
+
+const getPokemons = () => {
+    const pokemonsArr = Array.from( Array(650) )
+
+    return pokemonsArr.map( (_, index) => index + 1)
+}
+
+const getPokemonNames = async ([a, b, c, d]=[]) => {
+
+    const requests = [
+        pokemonApi.get(`/${ a }`),
+        pokemonApi.get(`/${ b }`),
+        pokemonApi.get(`/${ c }`),
+        pokemonApi.get(`/${ d }`)
+    ]
+
+    const [poke1, poke2, poke3, poke4] = await Promise.all(requests)
+
+    return [
+        { name: poke1.data.name, id: poke1.data.id },
+        { name: poke2.data.name, id: poke2.data.id },
+        { name: poke3.data.name, id: poke3.data.id },
+        { name: poke4.data.name, id: poke4.data.id }
+    ]
+}
+
+const getPokemonOptions = () => {
+    const mixed = getPokemons().sort(()=> Math.random() - 0.5)
+    return getPokemonNames(mixed.splice(0,4))
+}
+
+export default getPokemonOptions
